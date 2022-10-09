9d8cc7b9210cd7fb76214aa4fc9978a73de3b2c8
yujaemin <woawoa0513@gmail.com>
2022-10-09 18:52:40 +0900

    test: 회원 등록/삭제, 등급 기준 설정에 따른 등급 변경 테스트 케이스

--- src/Main.java
+++ src/Main.java
@@ -4,25 +4,38 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main_menu() {
+    public static void mainMenu() {
         System.out.println("1. 고객 관리(고객 정보 추가, 수정, 삭제)");
         System.out.println("2. 고객 리스트");
         System.out.println("3. 고객 분류 기준 설정");
     }
 
     public static void main(String[] args) throws CustomerException {
+        SmartStore smartStore = SmartStore.getInstance();
+        smartStore.setVIP_timeAndPayment(20, 50000);
+
         Customer c = new Customer();
+        System.out.println("객체 1 추가");
+
+        for (Customer cs : smartStore.getCustomers())
+            System.out.println(cs);
+        System.out.println("");
+
         Customer c2 = new Customer();
         Customer c3 = new Customer();
-        Customer c4 = new Customer();
+        System.out.println("객체 2,3 추가");
 
-        // 일련번호 자동생성 확인
-        System.out.println(c.getCustomer_serialNo());
-        System.out.println(c2.getCustomer_serialNo());
-        System.out.println(c3.getCustomer_serialNo());
-        System.out.println(c4.getCustomer_serialNo());
+        for (Customer cs : smartStore.getCustomers())
+            System.out.println(cs);
+        System.out.println("");
 
-        // 입력 성공 예시
+        smartStore.removeCustomer(c2);
+        System.out.println("객체 2 삭제");
+
+        for (Customer cs : smartStore.getCustomers())
+            System.out.println(cs);
+        System.out.println("");
+
         try {
             c.setCustomer_name("yujamint");
             c.setCustomer_id("abcde12345");
@@ -32,14 +45,14 @@ public class Main {
             System.out.println(e.getMessage());
         }
 
-        // 입력 실패 예시
-        try {
-            c.setCustomer_name("jam");
-            c.setCustomer_id("abcde12345");
-            c.setCustomer_spentTime(15);
-            c.setCustomer_totalPayment(-45);
-        } catch (CustomerException e) {
-            System.out.println(e.getMessage());
-        }
+        for (Customer cs : smartStore.getCustomers())
+            System.out.println(cs);
+        System.out.println("");
+
+        smartStore.setCustomers_memberGrade();
+
+        for (Customer cs : smartStore.getCustomers())
+            System.out.println(cs);
+        System.out.println("");
     }
 }
