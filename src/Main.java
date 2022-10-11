12051664ced10f8c1c1cb897e699e24b6f0c6164
yujaemin <woawoa0513@gmail.com>
2022-10-11 14:01:06 +0900

    test: Customer 정렬 후 출력 기능 테스트 케이스

--- src/Main.java
+++ src/Main.java
@@ -13,46 +13,60 @@ public class Main {
     public static void main(String[] args) throws CustomerException {
         SmartStore smartStore = SmartStore.getInstance();
         smartStore.setVIP_timeAndPayment(20, 50000);
+        smartStore.setVVIP_timeAndPayment(50, 150000);
 
-        Customer c = new Customer();
-        System.out.println("객체 1 추가");
-
-        for (Customer cs : smartStore.getCustomers())
-            System.out.println(cs);
-        System.out.println("");
-
+        Customer c1 = new Customer();
         Customer c2 = new Customer();
         Customer c3 = new Customer();
-        System.out.println("객체 2,3 추가");
+        Customer c4 = new Customer();
 
-        for (Customer cs : smartStore.getCustomers())
-            System.out.println(cs);
-        System.out.println("");
-
-        smartStore.removeCustomer(c2);
-        System.out.println("객체 2 삭제");
-
-        for (Customer cs : smartStore.getCustomers())
-            System.out.println(cs);
-        System.out.println("");
 
         try {
-            c.setCustomer_name("yujamint");
-            c.setCustomer_id("abcde12345");
-            c.setCustomer_spentTime(35);
-            c.setCustomer_totalPayment(100000);
+            c1.setCustomer_name("yujamint");
+            c1.setCustomer_id("testId1");
+            c1.setCustomer_spentTime(35);
+            c1.setCustomer_totalPayment(100000);
         } catch (CustomerException e) {
             System.out.println(e.getMessage());
         }
 
-        for (Customer cs : smartStore.getCustomers())
-            System.out.println(cs);
-        System.out.println("");
+        try {
+            c2.setCustomer_name("jamjam");
+            c2.setCustomer_id("testid2");
+            c2.setCustomer_spentTime(4);
+            c2.setCustomer_totalPayment(35000);
+        } catch (CustomerException e) {
+            System.out.println(e.getMessage());
+        }
 
+        try {
+            c3.setCustomer_name("dbwoals");
+            c3.setCustomer_id("testId3");
+            c3.setCustomer_spentTime(66);
+            c3.setCustomer_totalPayment(280000);
+        } catch (CustomerException e) {
+            System.out.println(e.getMessage());
+        }
+
+        try {
+            c4.setCustomer_name("woa");
+            c4.setCustomer_id("testd4");
+            c4.setCustomer_spentTime(81);
+            c4.setCustomer_totalPayment(140000);
+        } catch (CustomerException e) {
+            System.out.println(e.getMessage());
+        }
         smartStore.setCustomers_memberGrade();
 
-        for (Customer cs : smartStore.getCustomers())
-            System.out.println(cs);
+        smartStore.printCustomers();
         System.out.println("");
+
+        smartStore.sortByCustomerName();
+        System.out.println("");
+
+        smartStore.sortByCustomerSpentTIme();
+        System.out.println("");
+
+        smartStore.sortByCustomerTotalPayment();
     }
 }
